1404520cdc8e65d7a0ea4d8621966d73a969426b
marthesheila <marthe.gnitedemg@devoteamgcloud.com>
2022-11-03 15:10:27 +0100

    creating buckets using terraform

--- iac/cloud_storage.tf
+++ iac/cloud_storage.tf
@@ -5,7 +5,7 @@ resource "google_storage_bucket" "magasin_cie_landing" {
   lifecycle_rule {
     condition {
       age = 30
-      matches_prefix = "archive/"
+      matches_prefix = ["archive/"]
     }
     action {
       type = "SetStorageClass"
@@ -15,7 +15,7 @@ resource "google_storage_bucket" "magasin_cie_landing" {
   lifecycle_rule {
     condition {
       age = 90
-      matches_prefix = "archive/"
+      matches_prefix = ["archive/"]
     }
     action {
       type = "SetStorageClass"
@@ -25,7 +25,7 @@ resource "google_storage_bucket" "magasin_cie_landing" {
   lifecycle_rule {
     condition {
       age = 365
-      matches_prefix = "archive/"
+      matches_prefix = ["archive/"]
     }
     action {
       type = "SetStorageClass"
@@ -35,7 +35,7 @@ resource "google_storage_bucket" "magasin_cie_landing" {
   lifecycle_rule {
     condition {
       age = 1000
-      matches_prefix = "archive/"
+      matches_prefix = ["archive/"]
     }
     action {
       type = "Delete"
