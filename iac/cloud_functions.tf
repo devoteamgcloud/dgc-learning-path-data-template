c0ff9daae320474e252bbc9da57a43a0c42497ef
marthesheila <marthe.gnitedemg@devoteamgcloud.com>
2022-11-28 11:05:56 +0100

    fix(cloud_functions.tf): add variables 'project_id' and 'location'

--- iac/cloud_functions.tf
+++ iac/cloud_functions.tf
@@ -21,6 +21,8 @@ resource "google_storage_bucket_object" "zip" {
 }
 
 resource "google_cloudfunctions_function" "function" {
+    project  = var.project_id
+    location = var.location
     name                  = "function-trigger-on-gcs"
     runtime               = "python37"  # of course changeable
 
