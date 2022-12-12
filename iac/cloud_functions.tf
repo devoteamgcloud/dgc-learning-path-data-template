0b4e3a32101e68a33de4649b4624c28f7ac9201e
LHT-DVT <loic.hanot@devoteamgcloud.com>
2022-12-12 17:14:19 +0100

    wesh

--- iac/cloud_functions.tf
+++ iac/cloud_functions.tf
@@ -1,14 +1,4 @@
 /*
-variable "project_id" {
-    default = "sandbox-lhanot"
-}
-variable "region" {
-    default = "europe-west1"
-}
-provider "google" {
-  project = var.project_id
-  region  = var.region
-}
 resource "google_storage_bucket" "magasin_cie_landing" {
   project  = var.project_id
   name     = "${var.project_id}_magasin_cie_landing"
@@ -22,7 +12,7 @@ resource "google_storage_bucket" "magasin_cie_utils" {
 }*/
 data "archive_file" "source" {
     type        = "zip"
-    source_dir  = "./cloud_functions/cf_trigger_on_file/src"
+    source_dir  = "../cloud_functions/cf_trigger_on_file/src"
     output_path = "/tmp/function.zip"
 }
 
@@ -34,11 +24,11 @@ resource "google_storage_bucket_object" "zip" {
     # Append to the MD5 checksum of the files's content
     # to force the zip to be updated as soon as a change occurs
     name         = "src-${data.archive_file.source.output_md5}.zip"
-    bucket       = google_storage_bucket.function_bucket.name
+    bucket       = google_storage_bucket.cloud_functions_sources.name
 
     # Dependencies are automatically inferred so these lines can be deleted
     depends_on   = [
-        google_storage_bucket.function_bucket,  # declared in `storage.tf`
+        google_storage_bucket.cloud_functions_sources,  # declared in `storage.tf`
         data.archive_file.source
     ]
 }
@@ -49,7 +39,7 @@ resource "google_cloudfunctions_function" "function" {
     runtime               = "python310"  # of course changeable
 
     # Get the source code of the cloud function as a Zip compression
-    source_archive_bucket = google_storage_bucket.function_bucket.name
+    source_archive_bucket = google_storage_bucket.cloud_functions_sources.name
     source_archive_object = google_storage_bucket_object.zip.name
 
     # Must match the function name in the cloud function `main.py` source code
@@ -63,7 +53,7 @@ resource "google_cloudfunctions_function" "function" {
 
     # Dependencies are automatically inferred so these lines can be deleted
     depends_on            = [
-        google_storage_bucket.function_bucket,  # declared in `storage.tf`
+        google_storage_bucket.cloud_functions_sources,  # declared in `storage.tf`
         google_storage_bucket_object.zip
     ]
 }
