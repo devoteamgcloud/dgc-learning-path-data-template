ce3709b8e8c16ac260b5d43954ccb5ceb57199d3
marcyves-dgc <yves.marcel.ngabirano@devoteamgcloud.com>
2022-12-14 17:23:29 +0100

    fix(queries): fix up the query for the staging step

--- queries/staging/customer.sql
+++ queries/staging/customer.sql
@@ -3,7 +3,7 @@ SELECT
     first_name,
     UPPER(last_name)                            AS `last_name`,
     email,
-    PARSE_DATED("%d-%m-%Y", creation_date)      AS `creation_date`,
+    PARSE_DATE("%d-%B-%y", creation_date)      AS `creation_date`,
     update_time,
     CURRENT_TIMESTAMP()                         AS `insertion_time`
 FROM `{{ project_id }}.raw.customer`;
