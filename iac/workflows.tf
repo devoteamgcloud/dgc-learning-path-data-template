d592fe75d07c4ad9c61393e2b516313cbf11bfbf
marcyves-dgc <yves.marcel.ngabirano@devoteamgcloud.com>
2022-12-14 16:47:46 +0100

    feat(iac): adding the resource of the customer staging

--- iac/workflows.tf
+++ iac/workflows.tf
@@ -4,3 +4,10 @@ resource "google_workflows_workflow" "store_workflow" {
   region          = var.region
   source_contents = file("../cloud_workflows/store_wkf.yaml")
 }
+
+resource "google_workflows_workflow" "customer_staging_workflow" {
+  project = var.project_id
+  name = "customer_staging_wkf"
+  region = var.region
+  source_contents = file("../cloud_workflows/customer_staging_wkf.yaml")  
+}
