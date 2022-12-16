3d87c311f58244f0bfb0fb6e43219a31d361e0dc
marcyves-dgc <yves.marcel.ngabirano@devoteamgcloud.com>
2022-12-16 13:13:49 +0100

    feat(queries): perform a functional deduplication

--- queries/staging/customer.sql
+++ queries/staging/customer.sql
@@ -6,4 +6,11 @@ SELECT
     PARSE_DATE("%d-%B-%y", creation_date)      AS `creation_date`,
     update_time,
     CURRENT_TIMESTAMP()                         AS `insertion_time`
-FROM `{{ project_id }}.raw.customer`;
+FROM `{{ project_id }}.raw.customer`
+QUALIFY ROW_NUMBER() OVER(
+  PARTITION BY
+    id_customer
+  ORDER BY
+    update_time DESC,
+    creation_date DESC
+) = 1;
