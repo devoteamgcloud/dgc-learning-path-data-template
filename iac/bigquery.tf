67ce0a528df03a0b721e56554481071b08f114bc
LHT-DVT <loic.hanot@devoteamgcloud.com>
2023-01-31 15:32:05 +0100

    fix(bigquery.tf): change resources parameters

--- iac/bigquery.tf
+++ iac/bigquery.tf
@@ -13,13 +13,13 @@ resource "google_bigquery_dataset" "cleaned" {
 }
 
 resource "google_bigquery_table" "raw_store" {
-  dataset_id = var.raw
+  dataset_id = google_bigquery_dataset.raw
   table_id = var.raw_store
   schema = file("../schemas/raw/store.json")
 }
 
 resource "google_bigquery_table" "cleaned_store" {
-  dataset_id = var.cleaned
+  dataset_id = google_bigquery_dataset.cleaned
   table_id = var.cleaned_store
   schema = file("../schemas/cleaned/store.json")
 }
