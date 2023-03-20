f62f03850da62d8d54aa8586df3e65a6eb13ab57
VESTUArthur <vestuarthur@gmail.com>
2023-03-20 12:15:39 +0100

    chap 4 : terraform update

--- iac/workflows.tf
+++ iac/workflows.tf
@@ -3,16 +3,16 @@ resource "google_project_service" "workflows" {
   service            = "workflows.googleapis.com"
   disable_on_destroy = false
 }
-resource "google_service_account" "workflow_account" {
-  account_id   = "wkf-sa"
-  display_name = "Workflow Service Account"
+resource "google_service_account" "workflows_service_account" {
+  account_id   = "sample-workflows-sa"
+  display_name = "Sample Workflows Service Account"
 }
 resource "google_workflows_workflow" "store_workflow" {
   project = var.project_id
   name          = "store_wkf"
   region        = var.region
   description   = "Workflow source data"
-  service_account = google_service_account.workflow_account.account_id
+  service_account = google_service_account.workflows_service_account.id
   source_contents = file("../cloud_workflows/store_wkf.yaml")
   depends_on = [
     google_project_service.workflows
