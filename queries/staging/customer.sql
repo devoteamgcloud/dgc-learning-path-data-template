24fa5e151fd3931e34a88d9c376290139d831f25
VESTUArthur <vestuarthur@gmail.com>
2023-03-21 16:00:31 +0100

    chap 5 fixe parse date

--- queries/staging/customer.sql
+++ queries/staging/customer.sql
@@ -3,7 +3,7 @@ SELECT
     first_name,
     last_name,
     email,
-    PARSE_DATE("%d-%m-%Y", creation_date)    AS `creation_date`,
+    PARSE_DATE("%d-%b-%y", creation_date)    AS `creation_date`,
     update_time,
     CURRENT_TIMESTAMP()                      AS `insertion_time`
 FROM `{{ project_id }}.raw.customer`;
