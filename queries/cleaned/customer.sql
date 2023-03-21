cbe7240fcaae974672f01d10bc683164297c4548
VESTUArthur <vestuarthur@gmail.com>
2023-03-21 16:27:47 +0100

    chap5 fix query

--- queries/cleaned/customer.sql
+++ queries/cleaned/customer.sql
@@ -1,5 +1,25 @@
-MERGE `{{ project_id }}.cleaned.customer` T
-USING `{{ project_id }}.staging.customer` S
-ON T.id_customer = S.id_customer
-WHEN MATCHED THEN UPDATE SET *
-WHEN NOT MATCHED BY TARGET INSERT ROW
+MERGE
+  `sandbox-avestu.cleaned.customer` T
+USING
+  `sandbox-avestu.staging.customer` S
+ON
+  T.id_customer = S.id_customer
+  WHEN MATCHED THEN UPDATE SET 
+  T.id_customer = S.id_customer,
+    T.first_name = S.first_name,
+    T.last_name = S.last_name,
+    T.email = S.email,
+    T.creation_date = S.creation_date,
+    T.update_time = S.update_time,
+    T.insertion_time = S.insertion_time
+  WHEN NOT MATCHED BY TARGET THEN
+INSERT
+  (id_customer,
+    first_name,
+    last_name,
+    email,
+    creation_date,
+    update_time,
+    insertion_time)
+VALUES
+  (S.id_customer, S.first_name, S.last_name, S.email, S.creation_date, S.update_time, S.insertion_time);
