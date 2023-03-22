21b0a251fb4ebf7ca2dbae0b522939a38f02306a
VESTUArthur <vestuarthur@gmail.com>
2023-03-22 10:51:00 +0100

    chap 6 init

--- iac/bigquery.tf
+++ iac/bigquery.tf
@@ -16,6 +16,12 @@ resource "google_bigquery_dataset" "dataset_cleaned" {
   description                 = "This is the cleaned dataset"
   location                    = "EU"
 }
+resource "google_bigquery_dataset" "dataset_aggregated" {
+  project = var.project_id
+  dataset_id                  = "aggregateed"
+  description                 = "This is the aggregated dataset"
+  location                    = "EU"
+}
 
 resource "google_bigquery_table" "table_store_raw" {
   project = var.project_id
@@ -32,7 +38,16 @@ resource "google_bigquery_table" "table_store_cleaned" {
   schema =file("../schemas/cleaned/store.json")
   deletion_protection = false
 }
-
+resource "google_bigquery_table" "table_store_aggregated" {
+  project = var.project_id
+  dataset_id = google_bigquery_dataset.dataset_aggregated.dataset_id
+  table_id   = "open_store"
+  deletion_protection = false
+  view{
+    query = file("../queries/aggregated/open_store.sql")
+    use_legacy_sql = false
+  }
+}
 resource "google_bigquery_table" "table_customer_raw" {
   project = var.project_id
   dataset_id = google_bigquery_dataset.dataset_raw.dataset_id
@@ -56,3 +71,63 @@ resource "google_bigquery_table" "table_customer_cleaned" {
   schema =file("../schemas/cleaned/customer.json")
   deletion_protection = false
 }
+# resource "google_bigquery_table" "table_customer_purchase_aggregated" {
+#   project = var.project_id
+#   dataset_id = google_bigquery_dataset.dataset_aggregated.dataset_id
+#   table_id   = "customer_purchase"
+#   deletion_protection = false
+#   view{
+#     query = file("../queries/aggregated/customer_purchase.sql")
+#     use_legacy_sql = false
+#   }
+# }
+resource "google_bigquery_table" "table_basket_raw" {
+  project = var.project_id
+  dataset_id = google_bigquery_dataset.dataset_raw.dataset_id
+  table_id   = "basket"
+  schema =file("../schemas/raw/customer.json") 
+  deletion_protection = false
+}
+
+# resource "google_bigquery_table" "table_basket_staging" {
+#   project = var.project_id
+#   dataset_id = google_bigquery_dataset.dataset_staging.dataset_id
+#   table_id   = "basket"
+#   schema =file("../schemas/staging/customer.json") 
+#   deletion_protection = false
+# }
+# resource "google_bigquery_table" "table_basket_detail_staging" {
+#   project = var.project_id
+#   dataset_id = google_bigquery_dataset.dataset_staging.dataset_id
+#   table_id   = "basket_detail"
+#   schema =file("../schemas/staging/customer.json") 
+#   deletion_protection = false
+# }
+# resource "google_bigquery_table" "table_basket_detail_cleaned" {
+#   project = var.project_id
+#   dataset_id = google_bigquery_dataset.dataset_staging.dataset_id
+#   table_id   = "basket_detail"
+#   schema =file("../schemas/staging/customer.json") 
+#   deletion_protection = false
+# }
+# resource "google_bigquery_table" "table_basket_header_cleaned" {
+#   project = var.project_id
+#   dataset_id = google_bigquery_dataset.dataset_cleaned.dataset_id
+#   table_id   = "basket_header"
+#   schema =file("../schemas/cleaned/customer.json")
+#   deletion_protection = false
+# }
+# resource "google_bigquery_table" "table_day_sale" {
+#   project = var.project_id
+#   dataset_id = google_bigquery_dataset.dataset_aggregated.dataset_id
+#   table_id   = "day_sale"
+#   schema =file("../schemas/aggregated/day_sale.json") 
+#   deletion_protection = false
+# }
+# resource "google_bigquery_table" "table_best_product_sale" {
+#   project = var.project_id
+#   dataset_id = google_bigquery_dataset.dataset_aggregated.dataset_id
+#   table_id   = "best_product_sale"
+#   schema =file("../schemas/aggregated/best_product_sale.json")
+#   deletion_protection = false
+# }
---
+++ queries/aggregated/open_store.sql
@@ -0,0 +1,6 @@
+SELECT
+  city,
+  country,
+  coordinate,
+  creation_date
+FROM `{{ project_id }}.cleaned.store`;
