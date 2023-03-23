bcd813a8aeba2697e5b2bf8d173b20ea56cf1786
VESTUArthur <vestuarthur@gmail.com>
2023-03-23 15:08:25 +0100

    chap 6 fix staging basket_detail

--- queries/staging/basket_detail.sql
+++ queries/staging/basket_detail.sql
@@ -1,10 +1,10 @@
 SELECT
-  id_cash_desk,
+  id_basket_header,
   d.product_name AS product_name,
   d.quantity AS quantity,
   d.unit_price AS unit_price,
   update_time,
   CURRENT_TIMESTAMP() AS insertion_time
 FROM
-  `sandbox-avestu.raw.basket`,
+  `sandbox-avestu.staging.basket`,
   UNNEST(detail) AS d;
