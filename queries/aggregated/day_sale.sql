5ae8077e08b286e51ad8aa2a8fab03dae393b441
VESTUArthur <vestuarthur@gmail.com>
2023-03-24 08:49:29 +0100

    chap 6 fix typo

--- queries/aggregated/day_sale.sql
+++ queries/aggregated/day_sale.sql
@@ -1,7 +1,7 @@
 SELECT
   CAST(purchase_date AS DATE) AS day,
   SUM(n_product) AS total_product,
-  ROUND(SUM(total_price), 4)AS total_sales,
+  ROUND(SUM(total_price), 4)AS total_sale,
 FROM
   `sandbox-avestu.cleaned.basket_header`
   GROUP BY day
