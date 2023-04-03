be9c968e66213db2d6ecf3130cb2a80db3706a0c
amalchaabene23 <amal.chaabene@devoteamgcloud.com>
2023-04-03 16:40:06 +0200

    Added deduplication in table staging customer

--- queries/staging/customer.sql
+++ queries/staging/customer.sql
@@ -1,9 +1,23 @@
 SELECT 
   id_customer,
   first_name,
-  UPPER(last_name)                         AS `last_name`,
+  last_name,
   email,
-  PARSE_DATE("%d-%B-%y", creation_date)    AS `creation_date`,
+  creation_date,
   update_time,
   CURRENT_TIMESTAMP()                      AS `insertion_time`
-FROM `{{ project_id }}.raw.customer`;
+FROM (
+  SELECT 
+    id_customer,
+    first_name,
+    UPPER(last_name)                         AS `last_name`,
+    email,
+    PARSE_DATE("%d-%B-%y", creation_date)    AS `creation_date`,
+    update_time,
+    ROW_NUMBER() OVER (
+      PARTITION BY id_customer 
+      ORDER BY update_time DESC
+    ) AS row_num
+  FROM `sandbox-achaabene.raw.customer`
+) AS subquery
+WHERE row_num = 1;
