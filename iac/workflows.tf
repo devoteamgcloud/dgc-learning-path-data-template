88cbb6adfa4a2e232952ca57b8ba46635949e732
ValentinCordonnier <valentin.cordonnier@devoteamgcloud.com>
2023-04-03 16:28:32 +0200

    fix(iac): fix workflows

--- iac/workflows.tf
+++ iac/workflows.tf
@@ -1,5 +1,6 @@
 resource "google_workflows_workflow" "worflows" {  
   for_each        = fileset(path.module, "../{cloud_workflows}/**")
+  project         = var.project_id
   region          = "${var.location}"
   name            = trim(each.value, "../")
   source_contents = each.value
