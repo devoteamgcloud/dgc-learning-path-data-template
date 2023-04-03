21b48537a1be6fde8a66c322bc981374f2235d78
ValentinCordonnier <valentin.cordonnier@devoteamgcloud.com>
2023-04-03 10:09:37 +0200

    fix(iac): fix bigquery table raw

--- iac/bigquery.tf
+++ iac/bigquery.tf
@@ -1,1 +1,6 @@
-
+resource "google_bigquery_dataset" "dataset" {
+    dataset_id = "${var.project_id}_raw"
+    friendly_name = "raw"
+    description = "This is the raw table for Magasin&Cie"
+    location = "EU"
+}
