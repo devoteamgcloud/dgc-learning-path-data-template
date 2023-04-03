9fbe8acf05daf6557a591e02204d97d04bc5ab82
ValentinCordonnier <valentin.cordonnier@devoteamgcloud.com>
2023-04-03 16:36:04 +0200

    feat(iac): add workflows api

--- iac/workflows.tf
+++ iac/workflows.tf
@@ -1,3 +1,9 @@
+resource "google_project_service" "workflows" {
+  service            = "workflows.googleapis.com"
+  disable_on_destroy = false
+}
+
+
 resource "google_workflows_workflow" "worflows" {  
   for_each        = fileset(path.module, "../{cloud_workflows}/**")
   project         = var.project_id
