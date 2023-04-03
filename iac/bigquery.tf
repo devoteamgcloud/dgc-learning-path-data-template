b7277c5d808f2183325332208fcfdbb2a3463996
ValentinCordonnier <valentin.cordonnier@devoteamgcloud.com>
2023-04-03 10:25:29 +0200

    feat(iac): add table cleaned

--- iac/bigquery.tf
+++ iac/bigquery.tf
@@ -1,11 +1,11 @@
-resource "google_bigquery_dataset" "default" {
+resource "google_bigquery_dataset" "raw" {
     dataset_id = "raw"
     project = var.project_id
     description = "This is the raw table for Magasin&Cie"
     location = "EU"
 }
 
-resource "google_bigquery_dataset" "default" {
+resource "google_bigquery_dataset" "cleaned" {
     dataset_id = "cleaned"
     project = var.project_id
     description = "This is the cleaned table for Magasin&Cie"
