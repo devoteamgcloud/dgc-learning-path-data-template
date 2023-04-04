e61df2bfa6b523cc04692061979d676e411a37cd
cyrineselmene <cyrine.selmene@devoteamgcloud.com>
2023-04-04 10:39:28 +0200

    update workflow

--- iac/workflows.tf
+++ iac/workflows.tf
@@ -1,19 +1,19 @@
-# resource "google_project_service" "workflows" {
-#     service = "workflows.googleapis.com"
-#     disable_on_destroy = false
-# }
+ resource "google_project_service" "workflows" {
+     service = "workflows.googleapis.com"
+     disable_on_destroy = false
+ }
 
-# resource "google_service_account" "workflows_service_account" {
-#   account_id   = "sample-workflows-sa"
-#   display_name = "Sample Workflows Service Account"
-# }
+resource "google_service_account" "workflows_service_account" {
+  account_id   = "sample-workflows-sa"
+  display_name = "Sample Workflows Service Account"
+}
 
 resource "google_workflows_workflow" "workflows_store" {
   project = var.project_id
   name            = "store-workflow"
   region          = "europe-west1"
   description     = "A sample workflow"
-  #service_account = google_service_account.workflows_service_account.id
+  service_account = google_service_account.workflows_service_account.id
   source_contents = file("../cloud_workflows/store_wkf.yaml")
   depends_on = [google_project_service.workflows]
 }
