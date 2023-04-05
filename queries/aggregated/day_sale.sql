8f89069dd0d070a185334f168f10a1f4af86b93b
amalchaabene23 <amal.chaabene@devoteamgcloud.com>
2023-04-05 14:04:31 +0200

    Updated query for table day_sale

--- queries/aggregated/day_sale.sql
+++ queries/aggregated/day_sale.sql
@@ -1,6 +1,7 @@
 SELECT
-  DATE(purchase_date)             AS `day`,
-  n_product                       AS `total_product`,
-  ROUND(total_price, 2)           AS `total_sale`,
+  DATE(purchase_date)                  AS `day`,
+  SUM(n_product)                       AS `total_product`,
+  SUM(ROUND(total_price, 2))           AS `total_sale`,
 FROM
-  `{{ project_id }}.cleaned.basket_header`;
+  `{{ project_id }}.cleaned.basket_header`
+GROUP BY day;
