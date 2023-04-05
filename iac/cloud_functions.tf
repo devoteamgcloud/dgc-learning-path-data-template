9bd93011547757b413cce0cf71291ca1ff2d9cd2
ValentinCordonnier <valentin.cordonnier@devoteamgcloud.com>
2023-04-05 15:41:11 +0200

    fix(iac): fix trigger_on_file cloud function

--- iac/cloud_functions.tf
+++ iac/cloud_functions.tf
@@ -1,7 +1,8 @@
 resource "google_cloudfunctions_function" "trigger" {
   name        = "trigger_on_file"
+  project     = var.project_id
   description = "function that checks the file format and assign them to different folders"
   runtime     = "python310"
-  region      = "europe-west1"
+  region      = var.region
   entry_point = "check_file_format"
 }
