b52947bb3c65c632834fe7d3393569aab6f5eab9
cyrineselmene <cyrine.selmene@devoteamgcloud.com>
2023-04-06 17:27:40 +0200

    Fixed bug in cloud function file

--- iac/cloud_functions.tf
+++ iac/cloud_functions.tf
@@ -15,11 +15,11 @@ resource "google_storage_bucket_object" "zip" {
     # Append to the MD5 checksum of the files's content
     # to force the zip to be updated as soon as a change occurs
     name         = "src-${data.archive_file.source.output_md5}.zip"
-    bucket       = google_storage_bucket.function_bucket.name
+    bucket       = google_storage_bucket.cloud_functions_sources.name
 
     # Dependencies are automatically inferred so these lines can be deleted
     depends_on   = [
-        google_storage_bucket.function_bucket,  # declared in `storage.tf`
+        google_storage_bucket.cloud_functions_sources,  # declared in `storage.tf`
         data.archive_file.source
     ]
 }
@@ -30,7 +30,7 @@ resource "google_cloudfunctions_function" "function" {
     runtime               = "python37"  # of course changeable
 
     # Get the source code of the cloud function as a Zip compression
-    source_archive_bucket = google_storage_bucket.function_bucket.name
+    source_archive_bucket = google_storage_bucket.cloud_functions_sources.name
     source_archive_object = google_storage_bucket_object.zip.name
 
     # Must match the function name in the cloud function `main.py` source code
