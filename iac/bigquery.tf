b79ea2bdf255bf40bda9756d9e7b7bec168a5b4f
ValentinCordonnier <valentin.cordonnier@devoteamgcloud.com>
2023-04-12 16:41:12 +0200

    fix(iac): bigquery schemas

--- iac/bigquery.tf
+++ iac/bigquery.tf
@@ -30,48 +30,48 @@ resource "google_bigquery_table" "store_raw" {
   dataset_id = google_bigquery_dataset.raw.dataset_id
   table_id   = "store"
   project = var.project_id
-  schema = file("../schemas/raw/store.json")
-  deletion_protection = false
+  //schema = file("../schemas/raw/store.json")
+  //deletion_protection = false
 }
 
 resource "google_bigquery_table" "store_cleaned" {
   dataset_id = google_bigquery_dataset.cleaned.dataset_id
   table_id   = "store"
   project = var.project_id
-  schema = file("../schemas/cleaned/store.json")
-  deletion_protection = false
+  //schema = file("../schemas/cleaned/store.json")
+  //deletion_protection = false
 }
 
 resource "google_bigquery_table" "customer_raw" {
   dataset_id = google_bigquery_dataset.raw.dataset_id
   table_id   = "customer"
   project = var.project_id
-  schema = file("../schemas/raw/customer.json")
-  deletion_protection = false
+  //schema = file("../schemas/raw/customer.json")
+  //deletion_protection = false
 }
 
 resource "google_bigquery_table" "customer_staging" {
   dataset_id = google_bigquery_dataset.staging.dataset_id
   table_id   = "customer"
   project = var.project_id
-  schema = file("../schemas/staging/customer.json")
-  deletion_protection = false
+  //schema = file("../schemas/staging/customer.json")
+  //deletion_protection = false
 }
 
 resource "google_bigquery_table" "customer_cleaned" {
   dataset_id = google_bigquery_dataset.cleaned.dataset_id
   table_id   = "customer"
   project = var.project_id
-  schema = file("../schemas/cleaned/customer.json")
-  deletion_protection = false
+  //schema = file("../schemas/cleaned/customer.json")
+  //deletion_protection = false
 }
 
 resource "google_bigquery_table" "basket_raw" {
   dataset_id = google_bigquery_dataset.raw.dataset_id
   table_id   = "basket"
   project = var.project_id
-  schema = file("../schemas/raw/basket.json")
-  deletion_protection = false
+  //schema = file("../schemas/raw/basket.json")
+  //deletion_protection = false
 }
 
 resource "google_bigquery_table" "basket_staging" {
