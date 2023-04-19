717c5d059b7138b1979a299ce574fe5d19875ada
ValentinCordonnier <valentin.cordonnier@devoteamgcloud.com>
2023-04-19 09:38:51 +0200

    fix(queries): create view open_store and customer

--- queries/aggregated/customer_purchase.sql
+++ queries/aggregated/customer_purchase.sql
@@ -1,13 +1,15 @@
-SELECT
-  first_name,
-  last_name,
-  COUNT(id_basket_header) AS `n_basket`,
-  SUM(total_price) AS `total_purchase`,
-  MIN(purchase_date) AS `first_purchase_date`,
-  MAX(purchase_date) AS `last_purchase_date`
-FROM `{{ project_id }}.cleaned.customer` AS customer
-INNER JOIN `{{ project_id }}.cleaned.basket_header` AS basket_header
-  ON customer.id_customer = basket_header.id_customer
-GROUP BY
-  first_name,
-  last_name
+CREATE VIEW aggregated.customer_purchase(first_name, last_name, n_basket, total_purchase, first_purchase_date, last_purchase_date) AS (
+  SELECT
+    first_name,
+    last_name,
+    COUNT(id_basket_header) AS `n_basket`,
+    ROUND(SUM(total_price),2) AS `total_purchase`,
+    MIN(purchase_date) AS `first_purchase_date`,
+    MAX(purchase_date) AS `last_purchase_date`
+  FROM `{{ project_id }}.cleaned.customer` AS customer
+  INNER JOIN `{{ project_id }}.cleaned.basket_header` AS basket_header
+    ON customer.id_customer = basket_header.id_customer
+  GROUP BY
+    first_name,
+    last_name
+)
--- queries/aggregated/open_store.sql
+++ queries/aggregated/open_store.sql
@@ -1,7 +1,9 @@
-SELECT
-  city,
-  country,
-  coordinate,
-  creation_date
-FROM `{{ project_id }}.cleaned.store`
-WHERE is_closed = false
+CREATE VIEW aggregated.open_store(city, country, coordinate, creation_date) AS (
+  SELECT
+    city,
+    country,
+    coordinate,
+    creation_date
+  FROM `{{ project_id }}.cleaned.store`
+  WHERE is_closed = false
+)
