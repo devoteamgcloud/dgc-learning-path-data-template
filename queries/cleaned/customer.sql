69058a103988a3bb6d048a4e39747ce6f10aedbb
SeydinaDIOUF <seydina.mouhammad.diouf@gmail.com>
2023-04-27 11:06:50 +0200

    Chap5 -> build the query to MERGE staging.customer

---
+++ queries/cleaned/customer.sql
@@ -0,0 +1,30 @@
+-- MERGE <target table> [AS TARGET]
+-- USING <source table> [AS SOURCE]
+-- ON <search_condition>
+-- [WHEN MATCHED
+-- THEN <merge the matched records> ]
+-- [WHEN NOT MATCHED [BY TARGET]
+-- THEN <perform merge operations when matching record is not found in the target table> ]
+-- [WHEN NOT MATCHED BY SOURCE
+-- THEN <perform merge operations when matching record is not found in the source table> ];
+
+MERGE `{{ project_id }}.cleaned.customer` AS Target
+USING `{{ project_id }}.staging.customer` AS Source
+ON Source.id_customer = Target.id_customer
+WHEN NOT MATCHED BY Target THEN
+    INSERT (id_customer,
+    first_name,
+    last_name,
+    email,
+    creation_date,
+    update_time,
+    insertion_time)
+    VALUES (Source.id_customer,
+    Source.first_name,
+    Source.last_name,
+    Source.email,
+    Source.creation_date,
+    Source.update_time,
+    Source.insertion_time)
+WHEN MATCHED THEN
+    UPDATE SET Target.update_time = Source.update_time
