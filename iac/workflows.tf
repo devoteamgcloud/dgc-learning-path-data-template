f807d6c2d4cce1ce377cadf2855b21fd523b014d
SeydinaDIOUF <seydina.mouhammad.diouf@gmail.com>
2023-05-10 11:22:21 +0200

    Chap6 -> basket workflow

--- iac/workflows.tf
+++ iac/workflows.tf
@@ -13,3 +13,11 @@ resource "google_workflows_workflow" "customer_wkf" {
   description = "the Workflows customer_wkf"
   source_contents = file("../cloud_workflows/customer_wkf.yaml")
 }
+
+resource "google_workflows_workflow" "basket_wkf" {
+  project         = var.project_id
+  name            = "basket_wkf"
+  region          = var.region
+  description     = "the Workflows basket_wkf"
+  source_contents =  file("../cloud_workflows/basket_wkf.yaml")
+}
