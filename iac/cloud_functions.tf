82955e30425b169b06d32a20041bc8af3041f466
Mamadou-dian <mamadou.dian.djalo@devoteamgcloud.com>
2023-10-18 14:35:24 +0200

    Try-1 Cloud Storage trigger bucket sandbox-mdjalo-input

--- iac/cloud_functions.tf
+++ iac/cloud_functions.tf
@@ -41,7 +41,7 @@ resource "google_cloudfunctions_function" "function" {
 
   event_trigger {
     event_type          = "google.storage.object.finalize"
-    resource            = "${var.project_id}-input"
+    resource            = "${var.project_id}_magasin_cie_landing"
   }
 
   # Dependencies are automatically inferred so these lines can be deleted
