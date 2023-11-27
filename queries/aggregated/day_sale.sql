8b4e99e011706564ceabd76c603cb8cc3407450c
vvaneeclo <victor.vaneecloo@devoteamgcloud.com>
2023-11-27 16:36:17 +0100

    fix(day_sale.sql): bad understanding of the IC.
    **Features**
    - /
    
    **Fixes**
    - day_sale.sql works as intended in the Interface Contract.
    
    **Breaking Changes**
    /
    
    Issue #/

--- queries/aggregated/day_sale.sql
+++ queries/aggregated/day_sale.sql
@@ -1,8 +1,6 @@
 SELECT
-    EXTRACT(DATE FROM purchase_date) AS 'purchase_date' ,
-    SUM(n_product) AS 'total_product',
-    SUM(total_price) AS total_sale
+  DATE(purchase_date),
+  n_product           AS 'total_product',
+  total_price         AS total_sale
 FROM
   `{{ project_id }}.cleaned.basket_header`
-GROUP BY
-    1
