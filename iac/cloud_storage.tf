7fbc04d14d232e3a2ec8518d967ef1335da35022
vvaneeclo <victor.vaneecloo@devoteamgcloud.com>
2023-11-27 11:39:02 +0100

    fix(iac): locals.tf didn't work as intended.
    **Features**
    - Deleted locals.tf.
    - Recreated all locals in the corresponding _.tf file.
    
    **Fixes**
    - The infra will be deployed as intended.
    
    **Breaking Changes**
    /
    
    Issue #/

--- iac/cloud_storage.tf
+++ iac/cloud_storage.tf
@@ -1,5 +1,73 @@
-### BUCKETS ###
+### LOCALS ###
+locals {
+  ### BUCKETS ###
+  ### Bucket names ###
+  magasin_cie_landing    = "${var.project_id}_magasin_cie_landing"
+  magasin_cie_utils      = "${var.project_id}_magasin_cie_utils"
+  cloud_function_sources = "${var.project_id}_cloud_function_sources"
 
+  ### Bucket config ###
+  bucket_config = {
+    (local.magasin_cie_landing) = {
+      project  = var.project_id
+      location = var.location
+      lifecycle_rules = [
+        {
+          age                   = 30
+          matches_storage_class = ["STANDARD"]
+          matches_prefix        = "*/input/"
+          action_type           = "SetStorageClass"
+          storage_class         = "NEARLINE"
+        },
+        {
+          age                   = 90
+          matches_storage_class = ["NEARLINE"]
+          matches_prefix        = "*/input/"
+          action_type           = "SetStorageClass"
+          storage_class         = "COLDLINE"
+        },
+        {
+          age                   = 365
+          matches_storage_class = ["COLDLINE"]
+          matches_prefix        = "*/input/"
+          action_type           = "SetStorageClass"
+          storage_class         = "ARCHIVE"
+        },
+        {
+          age                   = 1000
+          matches_storage_class = ["ARCHIVE"]
+          matches_prefix        = "*/input/"
+          action_type           = "Delete"
+      }]
+    }
+    (local.magasin_cie_utils) = {
+      project  = var.project_id,
+      location = var.location
+    }
+    (local.cloud_function_sources) = {
+      project                     = var.project_id
+      name                        = "${var.project_id}_cloud_functions_sources"
+      location                    = var.location
+      force_destroy               = true
+      uniform_bucket_level_access = true
+    }
+  }
+
+  ### BUCKET OBJECT ###
+  ### Files ('/input' and "/invalid") ###
+  bucket_object_config = {
+      "input" = {
+      bucket  = local.magasin_cie_landing
+      content = " "
+      }
+      "invalid" = {
+      bucket  = local.magasin_cie_landing
+      content = " "
+      }
+  }
+}
+
+### BUCKETS ###
 resource "google_storage_bucket" "buckets" {
   for_each = local.bucket_config
   #### Required args ###
