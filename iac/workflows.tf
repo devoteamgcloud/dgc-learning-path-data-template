7c11d574bf577336d6a1d6278e3e4b2f6e7b014b
vvaneeclo <victor.vaneecloo@devoteamgcloud.com>
2023-11-28 18:46:19 +0100

    fix(cloud_workflows): each.name not compliant.
    **Features**
    - cloud_workflows/basket_wkf.yaml:
        - fixed indentation
    - iac/workflows.tf:
        - each.name >> split(".", each.name) to avoid errors
    
    **Fixes**
    - /
    
    **Breaking Changes**
    /
    
    Issue #/

--- iac/workflows.tf
+++ iac/workflows.tf
@@ -1,6 +1,6 @@
-resource "google_workflows_workflow" "store_wfk" {
+resource "google_workflows_workflow" "workflows" {
   for_each        = fileset("../cloud_workflows", "**")
-  name            = each.value
+  name            = split(".", each.value)[0]
   region          = "europe-west1"
   source_contents = file("../cloud_workflows/${each.value}")
 }
