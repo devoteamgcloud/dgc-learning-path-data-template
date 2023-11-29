7a3710b6002ef6fcf3022b2f36f6e0850a415b3e
vvaneeclo <victor.vaneecloo@devoteamgcloud.com>
2023-11-29 14:14:30 +0100

    fix(queries): ` instead of '
    **Features**
    - /
    
    **Fixes**
    - Changed all ' in ` to make queries work.
    
    **Breaking Changes**
    /
    
    Issue #/

--- queries/aggregated/customer_purchase.sql
+++ queries/aggregated/customer_purchase.sql
@@ -1,10 +1,10 @@
 SELECT
-  c.first_name               AS 'first_name',
-  c.last_name                AS 'last_name',
-  b.n_basket                 AS 'n_basket',
-  ROUND(b.total_purchase, 2) AS 'total_purchase',
-  b.first_purchase_date      AS 'first_purchase_date',
-  b.last_purchase_date       AS 'last_purchase_date'
+  c.first_name               AS `first_name`,
+  c.last_name                AS `last_name`,
+  b.n_basket                 AS `n_basket`,
+  ROUND(b.total_purchase, 2) AS `total_purchase`,
+  b.first_purchase_date      AS `first_purchase_date`,
+  b.last_purchase_date       AS `last_purchase_date`
 FROM
   `{{ project_id }}.cleaned.customer` c
   LEFT JOIN `{{ project_id }}.cleaned.basket_header` b ON c.id_customer = b.id_customer
--- queries/staging/customer.sql
+++ queries/staging/customer.sql
@@ -1,11 +1,11 @@
 SELECT
-  CAST(id_customer AS INTEGER) AS 'id_customer',
+  CAST(id_customer AS INTEGER) AS `id_customer`,
   first_name,
-  UPPER(last_name)             AS 'last_name',
+  UPPER(last_name)             AS `last_name`,
   email,
   CAST(creation_date           AS DATE),
   update_time,
-  CURRENT_TIMESTAMP()          AS 'insertion_time'
+  CURRENT_TIMESTAMP()          AS `insertion_time`
 FROM
   `{{ project_id }}.raw.customer`
 GROUP BY
