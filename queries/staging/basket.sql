2e066b18b9efd76c0d461a613539d103d28dcc0e
vvaneeclo <victor.vaneecloo@devoteamgcloud.com>
2023-12-08 10:35:30 +0100

    feat(basket.sql): finished query.
    **Features**
    - Finished writing and testing query.
    
    **Fixes**
    - /
    
    **Breaking Changes**
    /
    
    Issue #/

--- queries/staging/basket.sql
+++ queries/staging/basket.sql
@@ -1,85 +1,115 @@
-### S: raw.basket
--- id_cash_desk
--- id_customer
--- details
--- details.produ. ct_name
--- details.quantity
--- details.unit_price
--- payment_mode
--- purchase_date
--- update_time
+/*
+CONTEXT:
+- This query creates the staging.basket table in the basket workflow.
 
-### T: staging.basket
---> id_basket_header: incremental increase with ROW_NUMBER()
---> id_store: CAST(FIRST_PART(id_cash_desk) AS INT) -- FIRST_PART = TRIM + 
---> id_cash_desk: CAST(SECOND_PART(id_cash_desk) AS INT)
---> e.g. id_cash_desk: 00000012-00000789
-    --> id_store: 12
-    --> id_cash_desk: 789
--- id_customer
--- details
--- details.product_name
--- details.quantity
--- details.unit_price
--- payment_mode
--- purchase_date
--- update_time
---> insertion_time: NOW()
+RESULT EXPECTATION:
+- This query should create a staging.basket table with deduplicated `detail` subtable (by detail.product_name) and a `id_basket_header` coherent with the other tables in the workflow.
 
--- CREATE TABLE staging.basket_temp AS (
---   SELECT
---     NULL AS `id_basket_header`,
---     CAST(TRIM(SPLIT(id_cash_desk, "-")[0], "0") AS INT) AS id_store,
---     CAST(TRIM(SPLIT(id_cash_desk, "-")[1], "0") AS INT) AS id_cash_desk,
---     id_customer,
---     details,
---     payment_mode,
---     purchase_date,
---     update_time,
---     CURRENT_TIMESTAMP() AS `insertion_time`
---   FROM
---     `raw.basket`
--- );
+ASSUMPTION:
+- In the `UPDATE staging.basket` statement, the staging.basket_header.id_basket_header row needs to be NOT NULL in order to have the staging.basket.id_basket_header not always equal to 1.
+*/
 
-MERGE staging.basket_temp AS T
-USING (
-  WITH deduplicated_detail AS (
+/*
+Create the staging.basket_temp table without deduplication.
+*/
+CREATE OR REPLACE TABLE
+  staging.basket_temp AS (
     SELECT
-      -- PK
-      id_customer,
-      id_cash_desk,
+      NULL                                                 AS id_basket_header,
+      CAST(TRIM(SPLIT(id_cash_desk, "-") [0], "0") AS INT) AS id_store,
+      CAST(TRIM(SPLIT(id_cash_desk, "-") [1], "0") AS INT) AS id_cash_desk,
+      detail,
+      payment_mode,
       purchase_date,
-      -- details
-      STRUCT(
-          details.product_name AS product_name,
-          SUM(details.quantity) AS quantity,
-          ROUND(SUM(details.unit_price)/COUNT(details.product_name), 2) AS unit_price
-        ) AS detail
+      update_time,
+      CURRENT_TIMESTAMP()                                  AS `insertion_time`
     FROM
-      staging.basket_temp,
-      UNNEST(detail) AS details
-    GROUP BY
-      -- Group by PK & product_name to deduplicate
-      id_store,
-      id_customer,
+      `{{ project_id }}.raw.basket`
+  );
+
+/*
+Deduplicate the detail subtable (or nested table) from staging.basket_temp in place.
+*/
+MERGE
+  staging.basket_temp AS T USING (
+    -- CTE to deduplicate the `detail` subtable (or nested table) in place.
+    WITH
+      deduplicated_detail AS (
+        SELECT
+          -- PK
+          id_basket_header,
+          id_cash_desk,
+          purchase_date,
+          -- details
+          STRUCT (
+            details.product_name AS product_name,
+            SUM(details.quantity) AS quantity,
+            ROUND(
+              SUM(details.unit_price) / COUNT(details.product_name),
+              2
+            ) AS unit_price
+          ) AS detail
+        FROM
+          `{{ project_id }}.staging.basket_temp`,
+          UNNEST (detail) AS details
+        GROUP BY
+          -- Group by PK & product_name to deduplicate
+          id_store,
+          id_basket_header,
+          id_cash_desk,
+          purchase_date,
+          details.product_name
+      )
+      -- SELECT the new deduplicated detail FROM the `deduplicated_detail` CTE.
+    SELECT
+      id_basket_header,
       id_cash_desk,
       purchase_date,
-      details.product_name
-  )
-
-  SELECT
-    id_customer,
-    id_cash_desk,
-    purchase_date,
-    ARRAY_AGG(detail) AS detail
-  FROM
-    deduplicated_detail
-  GROUP BY   
-    id_customer,
-    id_cash_desk,
-    purchase_date
-) AS S
-ON T.id_customer = S.id_customer AND S.id_cash_desk = T.id_cash_desk AND S.purchase_date = T.purchase_date
+      ARRAY_AGG(detail) AS detail
+    FROM
+      deduplicated_detail
+    GROUP BY
+      id_basket_header,
+      id_cash_desk,
+      purchase_date
+  ) AS S ON T.id_basket_header = S.id_basket_header
+  AND S.id_cash_desk = T.id_cash_desk
+  AND S.purchase_date = T.purchase_date
 WHEN MATCHED THEN
-  UPDATE SET
-    T.detail = S.detail;
+UPDATE SET
+  detail = S.detail;
+
+/*
+Create table staging.basket by LEFT JOINING staging.basket_temp with cleaned.basket_header:
+- staging_basket_temp.* EXCEPT(id_basket_header): takes all columns but id_basket_header which we take from cleaned_basket_header.
+- LEFT JOIN on cleaned.basket_header PK: id_basket_header.
+*/
+CREATE OR REPLACE TABLE
+  staging.basket AS (
+    SELECT
+      cleaned_basket_header.id_basket_header,
+      staging_basket_temp.* EXCEPT           (id_basket_header)
+    FROM
+      `{{ project_id }}.staging.basket_temp` staging_basket_temp
+      LEFT JOIN `{{ project_id }}.cleaned.basket_header` cleaned_basket_header ON staging_basket_temp.id_basket_header = cleaned_basket_header.id_basket_header
+  );
+
+/*
+Update the staging.basket table in place:
+- COALESCE(MAX(id_basket_header), 0) + ROW_NUMBER() OVER (): Calculates the new header values by adding the maximum existing header (or 0 if none exist) to the sequential row numbers.
+
+Assumption:
+- The staging.basket_header table needs to be NOT NULL for this to work, otherwise, all id_basket_header will be equal to 1. 
+*/
+UPDATE staging.basket
+SET
+  id_basket_header = (
+    SELECT
+      COALESCE(MAX(id_basket_header), 0) + ROW_NUMBER() OVER ()
+    FROM
+      `{{ project_id }}.staging.basket`
+  )
+WHERE
+  id_basket_header IS NULL;
+
+DROP TABLE IF EXISTS `{{ project_id }}.staging.basket_temp`;
