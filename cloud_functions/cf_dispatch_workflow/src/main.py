f31e22902e21f6529da8a4233b454a3897e80671
Jules Rubin <58521821+julesrubin@users.noreply.github.com>
2025-03-15 09:00:15 +0100

    refactor(cf): clean up imports and improve code formatting

--- cloud_functions/cf_dispatch_workflow/src/main.py
+++ cloud_functions/cf_dispatch_workflow/src/main.py
@@ -1,20 +1,19 @@
+import base64
+import json
 import os
 import time
-import json
-import base64
 
-from google.cloud import storage
-from google.cloud import bigquery
+from google.cloud import bigquery, storage
 from google.cloud.workflows import executions_v1
 
 
 def receive_messages(event: dict, context: dict):
     """
     Triggered from a message on a Cloud Pub/Sub topic.
-    Inserts a file into the correct BigQuery raw table. If succedded then 
-    archive the file and trigger the Cloud Workflow pipeline else move the 
+    Inserts a file into the correct BigQuery raw table. If succedded then
+    archive the file and trigger the Cloud Workflow pipeline else move the
     file to the reject/ subfolder.
-    
+
     Args:
          event (dict): Event payload.
          context (google.cloud.functions.Context): Metadata for the event.
@@ -23,25 +22,25 @@ def receive_messages(event: dict, context: dict):
     # rename the variable to be more specific and write it to the logs
     pubsub_event = event
     print(pubsub_event)
-    
+
     # decode the data giving the targeted table name
-    table_name = base64.b64decode(pubsub_event['data']).decode('utf-8')
+    table_name = base64.b64decode(pubsub_event["data"]).decode("utf-8")
 
     # get the blob infos from the attributes
-    bucket_name = pubsub_event['attributes']['bucket_name']
-    blob_path = pubsub_event['attributes']['blob_path']
+    bucket_name = pubsub_event["attributes"]["bucket_name"]
+    blob_path = pubsub_event["attributes"]["blob_path"]
 
     load_completed = False
     try:
         # insert the data into the raw table then archive the file
         insert_into_raw(table_name, bucket_name, blob_path)
-        move_file(bucket_name, blob_path, 'archive')
+        move_file(bucket_name, blob_path, "archive")
         load_completed = True
-        
+
     except Exception as e:
         print(e)
-        move_file(bucket_name, blob_path, 'reject')
-        
+        move_file(bucket_name, blob_path, "reject")
+
     # trigger the pipeline if the load is completed
     if load_completed:
         trigger_worflow(table_name)
@@ -50,7 +49,7 @@ def receive_messages(event: dict, context: dict):
 def insert_into_raw(table_name: str, bucket_name: str, blob_path: str):
     """
     Insert a file into the correct BigQuery raw table.
-    
+
     Args:
          table_name (str): BigQuery raw table name.
          bucket_name (str): Bucket name of the file.
@@ -58,7 +57,7 @@ def insert_into_raw(table_name: str, bucket_name: str, blob_path: str):
     """
 
     # TODO: 2
-    # You have to try to insert the file into the correct raw table using the python BigQuery Library. 
+    # You have to try to insert the file into the correct raw table using the python BigQuery Library.
     # Please, refer yourself to the documentation and StackOverflow is still your friend ;)
     # As an help, you can follow those instructions:
     #     - connect to the Cloud Storage client
@@ -71,32 +70,29 @@ def insert_into_raw(table_name: str, bucket_name: str, blob_path: str):
     #     - (maybe you will need more variables according to the type of the file - csv, json - so it can be good to see the documentation)
     #     - and run your loading job from the blob uri to the destination raw table
     #     - waits the job to finish and print the number of rows inserted
-    # 
-    # note: this is not a small function. Take the day or more if you have to. 
+    #
+    # note: this is not a small function. Take the day or more if you have to.
 
     pass
 
-   
+
 def trigger_worflow(table_name: str):
     """
     Triggers and waits for a `<table_name>_wkf` Workflows pipeline's result within the project ID.
-    
+
     Args:
          table_name (str): BigQuery raw table name.
     """
-    
 
     # TODO: 3
-    # This is your final function to implement. 
-    # At this time, I hope you are more confortable with the Google Documentations for Python libraries. 
+    # This is your final function to implement.
+    # At this time, I hope you are more confortable with the Google Documentations for Python libraries.
     # So your are not guide except this little help:
     #     - trigger a Cloud Workflows execution according to the table updated
     #     - wait for the result (with exponential backoff delay will be better)
-    #     - be verbose where you think you have to 
-    
+    #     - be verbose where you think you have to
 
     raise NotImplementedError()
-
 
 
 def move_file(bucket_name, blob_path, new_subfolder):
@@ -110,11 +106,11 @@ def move_file(bucket_name, blob_path, new_subfolder):
     """
 
     # TODO: 1
-    # Now you are confortable with the first Cloud Function you wrote. 
+    # Now you are confortable with the first Cloud Function you wrote.
     # Inspire youreslf from this first Cloud Function and:
     #     - connect to the Cloud Storage client
     #     - get the bucket object and the blob object
-    #     - split the blob path to isolate the file name 
+    #     - split the blob path to isolate the file name
     #     - create your new blob path with the correct new subfolder given from the arguments
     #     - move you file inside the bucket to its destination
     #     - print the actual move you made
@@ -122,21 +118,20 @@ def move_file(bucket_name, blob_path, new_subfolder):
     pass
 
 
-if __name__ == '__main__':
-
+if __name__ == "__main__":
     # here you can test with mock data the function in your local machine
     # it will have no impact on the Cloud Function when deployed.
     import os
-    
-    project_id = '<YOUR-PROJECT-ID>'
+
+    project_id = "<YOUR-PROJECT-ID>"
 
     # test your Cloud Function for the store file.
     mock_event = {
-        'data': 'store'.encode('utf-8'),
-        'attributes': {
-            'bucket': f'{project_id}-magasin-cie-landing',
-            'file_path': os.path.join('input', 'store_20220531.csv'),
-        }
+        "data": "store".encode("utf-8"),
+        "attributes": {
+            "bucket": f"{project_id}-magasin-cie-landing",
+            "file_path": os.path.join("input", "store_20220531.csv"),
+        },
     }
 
     mock_context = {}
--- cloud_functions/cf_trigger_on_file/src/main.py
+++ cloud_functions/cf_trigger_on_file/src/main.py
@@ -1,63 +1,59 @@
-import os
 import datetime
-from google.cloud import storage
-from google.cloud import pubsub_v1
+import os
+
+from google.cloud import pubsub_v1, storage
 
 # This dictionary gives your the requirements and the specifications of the kind
-# of files you can receive. 
+# of files you can receive.
 #     - the keys are the names of the files
-#     - the values give the required extension for each file 
+#     - the values give the required extension for each file
 
-FILES_AND_EXTENSION_SPEC = {
-    'store': 'csv',
-    'customer': 'csv',
-    'basket': 'json'
-}
+FILES_AND_EXTENSION_SPEC = {"store": "csv", "customer": "csv", "basket": "json"}
 
 
 def check_file_format(event: dict, context: dict):
     """
     Triggered by a change to a Cloud Storage bucket.
-    Check for the files requirements. Publishes a message to PubSub if the 
+    Check for the files requirements. Publishes a message to PubSub if the
     file is verified else movs the files to the invalid/ subfolder.
 
     Args:
-         event (dict): Event payload. 
+         event (dict): Event payload.
                        https://cloud.google.com/storage/docs/json_api/v1/objects#resource-representations
          context (google.cloud.functions.Context): Metadata for the event.
     """
 
     # rename the variable to be more specific and write it to the logs
     blob_event = event
-    print(f'Processing blob: {blob_event["name"]}.')
+    print(f"Processing blob: {blob_event['name']}.")
 
     # get the bucket name and the blob path
-    bucket_name = blob_event['bucket']
-    blob_path = blob_event['name']
+    bucket_name = blob_event["bucket"]
+    blob_path = blob_event["name"]
 
     # get the subfolder, the file name and its extension
-    *subfolder, file = blob_path.split(os.sep)  
-    subfolder =  os.path.join(*subfolder) if subfolder != [] else ''
-    file_name, file_extention = file.split('.') 
+    *subfolder, file = blob_path.split(os.sep)
+    subfolder = os.path.join(*subfolder) if subfolder != [] else ""
+    file_name, file_extention = file.split(".")
 
-    print(f'Bucket name: {bucket_name}')
-    print(f'File path: {blob_path}')
-    print(f'Subfolder: {subfolder}')
-    print(f'Full file name: {file}')
-    print(f'File name: {file_name}')
-    print(f'File Extension: {file_extention}')
+    print(f"Bucket name: {bucket_name}")
+    print(f"File path: {blob_path}")
+    print(f"Subfolder: {subfolder}")
+    print(f"Full file name: {file}")
+    print(f"File name: {file_name}")
+    print(f"File Extension: {file_extention}")
 
     # Check if the file is in the subfolder `input/` to avoid infinite loop
-    assert subfolder == 'input', 'File must be in `input/ subfolder to be processed`'
-    
+    assert subfolder == "input", "File must be in `input/ subfolder to be processed`"
+
     # check if the file name has the good format
     # required format: <table_name>_<date>.<extension>
     try:
-        # TODO: 
+        # TODO:
         # create some assertions here to validate your file. It is:
         #     - required to have two parts.
         #     - the first part is required to be an accepted table name
-        #     - the second part is required to be a 'YYYYMMDD'-formatted date 
+        #     - the second part is required to be a 'YYYYMMDD'-formatted date
         #     - required to have the expected extension
 
         ...
@@ -66,11 +62,8 @@ def check_file_format(event: dict, context: dict):
 
         # if all checks are succesful then publish it to the PubSub topic
         publish_to_pubsub(
-            data=table_name.encode('utf-8'),
-            attributes={
-                'bucket_name': bucket_name, 
-                'blob_path': blob_path
-            }
+            data=table_name.encode("utf-8"),
+            attributes={"bucket_name": bucket_name, "blob_path": blob_path},
         )
 
     except Exception as e:
@@ -88,18 +81,17 @@ def publish_to_pubsub(data: bytes, attributes: dict):
          attributes (dict): Custom attributes for the message.
     """
     ## this small part is here to be able to simulate the function but
-    ## remove this part when you are ready to deploy your Cloud Function. 
+    ## remove this part when you are ready to deploy your Cloud Function.
     ## [start simulation]
-    print('Your file is considered as valid. It will be published to Pubsub.')
+    print("Your file is considered as valid. It will be published to Pubsub.")
     return
     ## [end simulation]
 
-
     # retrieve the GCP_PROJECT from the reserved environment variables
     # more: https://cloud.google.com/functions/docs/configuring/env-var#python_37_and_go_111
-    project_id = os.environ['GCP_PROJECT']
-    topic_id = os.environ['pubsub_topic_id']
-    
+    project_id = os.environ["GCP_PROJECT"]
+    topic_id = os.environ["pubsub_topic_id"]
+
     # connect to the PubSub client
     publisher = pubsub_v1.PublisherClient()
 
@@ -108,7 +100,8 @@ def publish_to_pubsub(data: bytes, attributes: dict):
     future = publisher.publish(topic_path, data, **attributes)
 
     print(future.result())
-    print(f'Published messages with custom attributes to {topic_path}.')
+    print(f"Published messages with custom attributes to {topic_path}.")
+
 
 def move_to_invalid_file_folder(bucket_name: str, blob_path: str):
     """
@@ -120,43 +113,41 @@ def move_to_invalid_file_folder(bucket_name: str, blob_path: str):
     """
 
     ## this small part is here to be able to simulate the function but
-    ## remove this part when you are ready to deploy your Cloud Function. 
+    ## remove this part when you are ready to deploy your Cloud Function.
     ## [start simulation]
-    print('Your file is considered as invalid. It will be moved to invalid/.')
+    print("Your file is considered as invalid. It will be moved to invalid/.")
     return
     ## [end simulation]
-    
-    
+
     # connect to the Cloud Storage client
     storage_client = storage.Client()
 
     # move the file to the invalid/ subfolder
     bucket = storage_client.bucket(bucket_name)
     blob = bucket.blob(blob_path)
-    new_blob_path = blob_path.replace('input', 'invalid')
+    new_blob_path = blob_path.replace("input", "invalid")
     bucket.rename_blob(blob, new_blob_path)
 
-    print(f'{blob.name} moved to {new_blob_path}')
+    print(f"{blob.name} moved to {new_blob_path}")
 
 
-if __name__ == '__main__':
-    
+if __name__ == "__main__":
     # here you can test with mock data the function in your local machine
     # it will have no impact on the Cloud Function when deployed.
     import os
-    
-    project_id = '<YOUR-PROJECT-ID>'
+
+    project_id = "<YOUR-PROJECT-ID>"
 
     realpath = os.path.realpath(__file__)
-    material_path = os.sep.join(['', *realpath.split(os.sep)[:-4], '__materials__'])
-    init_files_path = os.path.join(material_path, 'data', 'init')
+    material_path = os.sep.join(["", *realpath.split(os.sep)[:-4], "__materials__"])
+    init_files_path = os.path.join(material_path, "data", "init")
 
     # test your Cloud Function with each of the given files.
     for file_name in os.listdir(init_files_path):
-        print(f'\nTesting your file {file_name}')
+        print(f"\nTesting your file {file_name}")
         mock_event = {
-            'bucket': f'{project_id}-magasin-cie-landing',
-            'name': os.path.join('input', file_name)
+            "bucket": f"{project_id}-magasin-cie-landing",
+            "name": os.path.join("input", file_name),
         }
 
         mock_context = {}
